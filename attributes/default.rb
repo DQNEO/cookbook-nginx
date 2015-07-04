40376707b03a6f0640c284eb7092161b555d80fd
DQNEO <dqneoo@gmail.com>
2015-07-05 00:10:15 +0900

    use attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,6 @@ default['nginx']['root'] = "/usr/share/nginx/html"
 default['nginx']['server_name'] = "localhost"
 default['nginx']['sendfile'] = 'on'
 default['nginx']['user'] = 'nginx'
+default['nginx']['worker_processes'] = 1
+default['nginx']['worker_connections'] = 1024
+
